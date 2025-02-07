6a02b8579a038d1afe6458daf0a909cdf7e16ae0
amelialei <aclei@ucsd.edu>
2025-02-07 15:58:08 -0800

    fixed legend color

--- projects/projects.js
+++ projects/projects.js
@@ -5,18 +5,19 @@ let projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
 
-const legend = d3.select('.legend');
-const colors = d3.scaleOrdinal(d3.schemeTableau10);
-let svg = d3.select('svg');
-
-let selectedIndex = -1;
 
 function filterProjectsByYear(projects, year) {
     return projects.filter(project => project.year === year);
   }
 
+let selectedIndex = -1;
+
 
 function renderPieChart(projectsGiven) {
+  let legend = d3.select('.legend');
+  const colors = d3.scaleOrdinal(d3.schemeTableau10);
+  let svg = d3.select('svg');
+
   svg.selectAll('path').remove();
   legend.selectAll('li').remove();
 
@@ -34,39 +35,49 @@ function renderPieChart(projectsGiven) {
   let sliceGenerator = d3.pie().value((d) => d.value);
   let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
   let arcData = sliceGenerator(data);
+  let arcs = arcData.map((d) => arcGenerator(d));
 
-arcData.forEach((d, idx) => {
+  arcs.forEach((arc, i) => {
+    // console.log(d);
+    // console.log(idx);
     svg.append('path')
-      .attr('d', arcGenerator(d))
-      .attr('fill', colors(idx)) 
+      .attr('d', arc)
+      .attr('fill', colors(i)) 
       .style('cursor', 'pointer') 
       .on('click', function () {
-        selectedIndex = selectedIndex === idx ? -1 : idx;
+        selectedIndex = selectedIndex === i ? -1 : i;
+        console.log(selectedIndex);
   
+        legend.selectAll('li')
+        .attr('class', (_, idx) => idx === selectedIndex ? 'selected' : '');
+        
+        console.log(legend.selectAll('li'));
         svg.selectAll('path')
-          .attr('class', (_, i) => i === selectedIndex ? 'selected' : '');
-          if (selectedIndex === -1) {
-            renderProjects(projects, projectsContainer, 'h2'); 
-          } else {
-            let selectedYear = data[selectedIndex].label; 
-            let filteredProjects = filterProjectsByYear(projects, selectedYear); 
-            renderProjects(filteredProjects, projectsContainer, 'h2'); 
-          }
+          .attr('class', (_, idx) => idx === selectedIndex ? 'selected' : '');
+          
+        if (selectedIndex === -1) {
+          renderProjects(projects, projectsContainer, 'h2'); 
+        } else {
+          let selectedYear = data[selectedIndex].label; 
+          filteredProjects = projects.filter((p) => p.year === selectedYear);
+          // let filteredProjects = filterProjectsByYear(projects, selectedYear); 
+          renderProjects(filteredProjects, projectsContainer, 'h2'); 
+        }
+
       });
+
+    
   });
   
   data.forEach((d, idx) => {
     legend.append('li')
       .attr('style', `--color:${colors(idx)}`)
-      .html(`<span class="swatch" style="background-color:${colors(idx)};"></span> ${d.label} <em>(${d.value})</em>`)
-      .on('click', function () {
-        selectedIndex = selectedIndex === idx ? -1 : idx;
-  
-        svg.selectAll('path')
-          .attr('class', (_, i) => i === selectedIndex ? 'selected' : '');
-      });
+      .attr('class', `legend-item ${idx === selectedIndex ? 'selected' : ''}`)
+      .html(`<span class="swatch"></span>${d.label} <em>(${d.value})</em>`);
+      html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`);
   });
 }
+
 
 renderPieChart(projects);
 
