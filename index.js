0ca469b3d13a4352621a86a4988ef728a88ecf7f
amelialei <aclei@ucsd.edu>
2025-05-18 03:19:50 -0700

    added more projects

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import { fetchJSON, renderProjects, fetchGitHubData} from './global.js';
 
-const projects = await fetchJSON('./lib/projects.json');
-const latestProjects = projects.slice(0, 3);
+// const projects = await fetchJSON('./lib/projects.json');
+// const latestProjects = projects.slice(0, 3);
 
-const projectsContainer = document.querySelector('.projects');
+// const projectsContainer = document.querySelector('.projects');
 
-renderProjects(latestProjects, projectsContainer, 'h3');
+// renderProjects(latestProjects, projectsContainer, 'h3');
 
 const githubData = await fetchGitHubData('amelialei');
 
